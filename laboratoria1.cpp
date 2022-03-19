40fac21796c3fb72517e2b1407ba57aab40ba022
pplina <paulinapirog0@gmail.com>
2022-03-19 15:03:21 +0100

    Laboratorium 1

--- laboratoria1.cpp
+++ laboratoria1.cpp
@@ -3,32 +3,27 @@
 #include <vector>
 using namespace std;
 
-double S(int n, int i, double S0, double U, double D)
-{
+double S(int n, int i, double S0, double U, double D){
     return S0*pow(1+U,i)*pow(1+D,n-i);
 }
 
-void GetInputData(double &S0,double &U,double &D)
-{
+void GetInputData(double &S0,double &U,double &D){
     cout<<"S0= "; cin>>S0;
     cout<<"U = "; cin>>U;
     cout<<"D = "; cin>>D;
 }
 
-double RiskNeutralProb(double U, double D, double R)
-{
+double RiskNeutralProb(double U, double D, double R){
     return (R-D)/(U-D);
 }
 
-double CallPayoff(double S, double K)
-{
+double CallPayoff(double S, double K){
     if(S<K) return 0;
     return S-K;
 }
 
 double PriceByCRR(double S0, double U,double D, double R,
-                 int N, double K, double(*Payoff)(double S, double K))
-{
+                 int N, double K, double(*Payoff)(double S, double K)){
     vector<double> H(N+1);
     double q = RiskNeutralProb(U,D,R);
     for(int i=0;i<N+1;i++) H[i]=Payoff(S(N,i,S0,U,D),K);
@@ -40,8 +35,7 @@ double PriceByCRR(double S0, double U,double D, double R,
     return H[0];
 }
 
-int main()
-{
+int main(){
     double S0=100, U=0.1, D=-0.1, R=0.05;
     int n=2, i=1, N=3;
     double K=105;
